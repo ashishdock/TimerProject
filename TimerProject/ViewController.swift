612ccdcbb320cadce6f01987317f473633fbbbc5
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-22 13:39:37 +0530

    Done till thread blocking user interface.

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        for time in 1...10 {
+//            timeLabel.text = "Time: \(counter)"
+//            counter += 1
+//            print(time)
+//            Thread.sleep(forTimeInterval: 1)
+//        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        for time in 1...10 {
+            counter += 1
+            timeLabel.text = "Time: \(counter)"
+            print("Counter: \(counter)")
+            print("Time: \(time)")
+            Thread.sleep(forTimeInterval: 1) // User interface stops updating/ get blocked in realtime because of this.
+        }
     }
 
 
+    @IBAction func startButtonClicked(_ sender: UIButton) {
+        print("start button clicked")
+    }
 }
 
