812a6cf1a65d3152ddc47ed70107ac2640718c03
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-22 13:15:51 +0530

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Ashish Sharma on 12/22/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
