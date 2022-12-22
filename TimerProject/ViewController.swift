40a3217a09c4a506008317af0556fd319b74bc53
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-22 14:05:12 +0530

    TimerScheduler added user interface is not blocked.

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        counter = 10
+        timeLabel.text = "Time: \(counter)"
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerFunction), userInfo: nil, repeats: true)
+        
+        
+        // This block will not show up as Thread.sleep blocks viewDidAppear for 10 seconds.
 //        for time in 1...10 {
 //            timeLabel.text = "Time: \(counter)"
 //            counter += 1
@@ -24,15 +31,31 @@ class ViewController: UIViewController {
 //        }
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        for time in 1...10 {
-            counter += 1
-            timeLabel.text = "Time: \(counter)"
-            print("Counter: \(counter)")
-            print("Time: \(time)")
-            Thread.sleep(forTimeInterval: 1) // User interface stops updating/ get blocked in realtime because of this.
+    @objc func TimerFunction() {
+        counter -= 1
+        if counter == 0 {
+            print("counter has become ZERO")
+            timer.invalidate()
+            timeLabel.text = "Time's Over!"
+        } else {
+        
+        timeLabel.text = "Time: \(counter)"
+        print(counter)
         }
+        
+        
+        
     }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        for time in 1...10 {
+//            counter += 1
+//            timeLabel.text = "Time: \(counter)"
+//            print("Counter: \(counter)")
+//            print("Time: \(time)")
+//            Thread.sleep(forTimeInterval: 1) // User interface stops updating/ get blocked in realtime because of this.
+//        }
+//    }
 
 
     @IBAction func startButtonClicked(_ sender: UIButton) {
